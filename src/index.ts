ed3c78fb43eb1e4844141c9040e93a996c9d8140
EDWINCSN <c.edwin32@hotmail.com>
2026-09-21 22:30:14 -0500

    feat: crear servidor Express inicial

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express from "express";
+
+const app=express();
+
+const PUERTO=3001;
+
+app.use(express.json());
+//app.use("/albumes",albumesRouter)
+//app.use("/canciones",cancionesRouter)
+
+app.listen(PUERTO,()=>{
+    console.log("servidor corriendo en el puerto : "+PUERTO)
+})
+
